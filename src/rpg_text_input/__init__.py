f125dac01b9bb065a98549c7e2c67cea89f9ed8f
MeGaGiGaGon <107241144+MeGaGiGaGon@users.noreply.github.com>
2025-08-10 07:58:07 -0700

    Add basic on-screen keyboard rendering to RPG style input

--- src/rpg_text_input/__init__.py
+++ src/rpg_text_input/__init__.py
@@ -1,6 +1,34 @@
+from dataclasses import dataclass, field
+
 from nicegui import ui
 
 
+@dataclass
+class Keyboard:
+    """On-screen keyboard input method."""
+
+    keys: tuple[str, ...] = (
+        "ABCDEFGabcdefg",
+        "HIJKLMNhijklmn",
+        "OPQRSTUopqrstu",
+        "VWXYZ. vwxyz,\N{SQUARED OK}",
+    )
+
+    position: list[int] = field(default_factory=lambda: [0, 0])
+
+    def render(self) -> None:
+        """Render the keyboard to the page."""
+        with ui.grid(columns=len(self.keys[0])):
+            for row_index, row in enumerate(self.keys):
+                for col_index, char in enumerate(row):
+                    label = ui.label(char if char != "`" else "")
+                    label.style("text-align: center")
+                    if [col_index, row_index] == self.position:
+                        label.style("background-color: lightblue")
+
+
 @ui.page("/controller")
-def sub_page():
-    ui.label("test")
+def rpg_text_input_page() -> None:
+    """Page for the RPG style keyboard."""
+    keyboard = Keyboard()
+    keyboard.render()
