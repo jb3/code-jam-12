31f037c219b21d3877168237c5582c80c9289ee9
MeGaGiGaGon <107241144+MeGaGiGaGon@users.noreply.github.com>
2025-08-10 08:06:09 -0700

    Add selector movement to RPG style input

--- src/rpg_text_input/__init__.py
+++ src/rpg_text_input/__init__.py
@@ -1,6 +1,27 @@
 from dataclasses import dataclass, field
 
 from nicegui import ui
+from nicegui.events import KeyEventArguments
+
+
+def wrap_to_range(num: int, num_min: int, num_max: int) -> int:
+    """Ensure num is in the half-open interval [min, max), wrapping as needed. Min must be less than max.
+
+    Returns:
+        The input num wrapped to the given range.
+
+    Raises:
+        ValueError: If min is greater than or equal to max.
+
+    """
+    if num_min < num_max:
+        msg = f"Wrapping doesn't make sense if min >= max, got {num_min=} and {num_max=}."
+        raise ValueError(msg)
+    while num < num_min:
+        num += num_max
+    while num >= num_max:
+        num -= num_max
+    return num
 
 
 @dataclass
@@ -16,6 +37,7 @@ class Keyboard:
 
     position: list[int] = field(default_factory=lambda: [0, 0])
 
+    @ui.refreshable_method
     def render(self) -> None:
         """Render the keyboard to the page."""
         with ui.grid(columns=len(self.keys[0])):
@@ -26,9 +48,32 @@ class Keyboard:
                     if [col_index, row_index] == self.position:
                         label.style("background-color: lightblue")
 
+    def move(self, x: int, y: int) -> None:
+        """Move the keyboard selected character in the given directions."""
+        self.position[0] = wrap_to_range(self.position[0] + x, 0, len(self.keys[0]))
+        self.position[1] = wrap_to_range(self.position[1] + y, 0, len(self.keys))
+        self.render.refresh()
+
 
 @ui.page("/controller")
 def rpg_text_input_page() -> None:
     """Page for the RPG style keyboard."""
     keyboard = Keyboard()
     keyboard.render()
+
+    def handle_key(e: KeyEventArguments) -> None:
+        """Input handler for the RPG style keyboard."""
+        if not e.action.keydown:
+            return
+
+        # Done using a for loop to minimize copy/paste errors
+        for key_codes, direction in (
+            ({"KeyW", "ArrowUp"}, (0, -1)),
+            ({"KeyS", "ArrowDown"}, (0, 1)),
+            ({"KeyA", "ArrowLeft"}, (-1, 0)),
+            ({"KeyD", "ArrowRight"}, (1, 0)),
+        ):
+            if e.key.code in key_codes:
+                keyboard.move(*direction)
+
+    ui.keyboard(on_key=handle_key)
