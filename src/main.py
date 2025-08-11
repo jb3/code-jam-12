a3cff30afdcfd70327bb05e9c5e3fd13f68a6b1e
afx <arcticfox8732@gmail.com>
2025-08-11 02:49:53 -0500

    Update main.py
    i forgot to import

--- src/main.py
+++ src/main.py
@@ -1,8 +1,10 @@
 from nicegui import ui
 # We probably want to figure out a more clean way to do this without the noqa.
 import rpg_text_input as _  # noqa: F401 Importing creates the subpage.
+import wpm_tester
 
 ui.label("Hello NiceGUI!")
 ui.page("/test/{method}")(wpm_tester.wpm_tester_page)
 
 ui.run()
+
