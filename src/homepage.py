016d17faf1b12b9433c004aceadf0160813c2b39
enskyeing <skyetsatsis@gmail.com>
2025-08-11 20:13:48 -0700

    Define color theme

--- src/homepage.py
+++ src/homepage.py
@@ -7,6 +7,7 @@ DESCRIPTION: str = "Placeholder Description"
 
 @ui.page('/')
 def home() -> None:
+    ui.colors(primary="#20A39E", secondary="#8E7DBE", dark="#393D3F", accent="#E9ECF5")
     pass
 
 
