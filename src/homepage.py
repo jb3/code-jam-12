548db4b33058843122e357964097bc5ed98396f4
enskyeing <skyetsatsis@gmail.com>
2025-08-11 20:20:04 -0700

    Revert "Define color theme"
    This reverts commit 882cebdf9ddfaafae846aa076e7928fa04c4b8b7.

--- src/homepage.py
+++ src/homepage.py
@@ -7,7 +7,6 @@ DESCRIPTION: str = "Placeholder Description"
 
 @ui.page('/')
 def home() -> None:
-    ui.colors(primary="#20A39E", secondary="#8E7DBE", dark="#393D3F", accent="#E9ECF5")
     pass
 
 
