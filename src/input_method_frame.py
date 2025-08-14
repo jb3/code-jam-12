548525811f3887e30515d53e2f1c9bf3afc5141e
enskyeing <skyetsatsis@gmail.com>
2025-08-14 00:01:54 -0700

    Create header layout

--- src/input_method_frame.py
+++ src/input_method_frame.py
@@ -1,15 +1,42 @@
 from nicegui import ui
 
-import input_method_proto
+NAME: str = "PLACEHOLDER NAME"
+DESCRIPTION: str = "Placeholder Description"
 
 
-def input_method_page(input_method: input_method_proto.IInputMethod) -> None:
+@ui.page("/")
+def input_method_page() -> None:
     """User interface frame for input method pages.
 
     Args:
         input_method: The input method to be generated on the page.
 
     """
+    ui.add_css("""
+        .header {
+            height: 8vh;
+            align-items: center;
+        }
+        .h1 {
+            font-family: Arial;
+            font-size: 35px;
+            font-weight: bold;
+        }
+    """)
+    with ui.header(wrap=False).style("background-color: #20A39E").classes("items-center justify-between header"):
+        with ui.card().props("flat"):  # small logo placeholder
+            pass
+        ui.label(NAME).classes("h1")
+        ui.button(on_click=lambda: right_drawer.toggle(), icon="menu").props("flat color=white")
+    with (
+        ui.right_drawer(
+            value=False,
+            fixed=False,
+        )
+        .style("background-color: #ebf1fa")
+        .props("bordered overlay") as right_drawer
+    ):
+        ui.label("HOME")
 
 
 ui.run()
