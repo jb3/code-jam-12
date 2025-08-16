54b9f7de52cc63e014e8face9d6f77e95c30f855
enskyeing <skyetsatsis@gmail.com>
2025-08-16 12:20:59 -0700

    Add components to input methods

--- src/config.py
+++ src/config.py
@@ -1,3 +1,6 @@
+import audio_style_input
+import rpg_text_input
+
 PROJECT_NAME: str = "Dynamic Typing"
 PROJECT_DESCRIPTION: str = "How fast can you type?"
 
@@ -7,21 +10,25 @@ INPUT_METHODS: list[dict] = [
         "name": "Record Player",
         "path": "/audio-input",
         "icon": "",
+        "component": audio_style_input,
     },
     {
         "name": "WASD",
         "path": "/wasd",
         "icon": "",
+        "component": rpg_text_input,
     },
     {
         "name": "Color Picker",
         "path": "/color-picker",
         "icon": "",
+        "component": "",
     },
     {
         "name": "Circle Selector",
         "path": "/circle-selector",
         "icon": "",
+        "component": "",
     },
 ]
 
