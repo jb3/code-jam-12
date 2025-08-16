2d0fd310f3ea462061cdc31220da6e604a9a8406
enskyeing <skyetsatsis@gmail.com>
2025-08-15 22:23:36 -0700

    Add config.py

---
+++ config.py
@@ -0,0 +1,34 @@
+PROJECT_NAME: str = "Dynamic Typing"
+PROJECT_DESCRIPTION: str = "How fast can you type?"
+
+# INPUT METHODS
+INPUT_METHODS: list[dict] = [
+    {
+        "name": "Record Player",
+        "path": "/record-player",
+        "icon": "",
+    },
+    {
+        "name": "WASD",
+        "path": "/wasd",
+        "icon": "",
+    },
+    {
+        "name": "Color Picker",
+        "path": "/color-picker",
+        "icon": "",
+    },
+    {
+        "name": "Circle Selector",
+        "path": "/circle-selector",
+        "icon": "",
+    },
+]
+
+# COLORS
+COLOR_STYLE: dict = {
+    "PRIMARY": "",
+    "SECONDARY": "",
+    "PRIMARY_BG": "",
+    "SECONDARY_BG": "",
+}
