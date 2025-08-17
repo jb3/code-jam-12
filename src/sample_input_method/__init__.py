68d78b207130ab327dbe5dc24159b4d746b33051
MeGaGiGaGon <107241144+MeGaGiGaGon@users.noreply.github.com>
2025-08-16 22:52:15 -0700

    Name text callback type

--- src/sample_input_method/__init__.py
+++ src/sample_input_method/__init__.py
@@ -1,5 +1,3 @@
-import typing
-
 from nicegui import ui
 
 import input_method_proto
@@ -11,13 +9,13 @@ class SampleInputMethod(input_method_proto.IInputMethod):
     Consider using a dataclass instead with any complex state.
     """
 
-    callbacks: list[typing.Callable[[str], None]]
+    callbacks: list[input_method_proto.TextUpdateCallback]
 
     def __init__(self) -> None:
         self.callbacks = []
         self.inp = ui.input("input here")
         self.inp.on_value_change(lambda event: [x(event.value) for x in self.callbacks])
 
-    def on_text_update(self, callback: typing.Callable[[str], None]) -> None:
+    def on_text_update(self, callback: input_method_proto.TextUpdateCallback) -> None:
         """Call `callback` every time the user input changes."""
         self.callbacks.append(callback)
