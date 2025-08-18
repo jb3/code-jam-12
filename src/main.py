198ef8f4335b6ef1da5716b61d6b3d19556695d7
MeGaGiGaGon <107241144+MeGaGiGaGon@users.noreply.github.com>
2025-08-18 00:16:25 -0700

    Clean up comments in main.py

--- src/main.py
+++ src/main.py
@@ -3,13 +3,7 @@ from nicegui import ui
 from homepage import home
 from wpm_tester import wpm_tester_page
 
-###from rpg_text_input import rpg_text_input_page so it doesnt think it's code
-
-
 ui.page("/")(home)
 ui.page("/test/{method}")(wpm_tester_page)
-
-# http://localhost:8080/test/audio_input
-
 
 ui.run()
