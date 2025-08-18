51a439baabfdfa07343bdaadfd47564acd88e4c2
enskyeing <skyetsatsis@gmail.com>
2025-08-18 02:09:06 -0700

    Remove path from main

--- src/main.py
+++ src/main.py
@@ -1,12 +1,7 @@
-from pathlib import Path
-
-from nicegui import app, ui
+from nicegui import ui
 
 from homepage import home
 from wpm_tester import wpm_tester_page
-
-media = Path("./static")
-app.add_media_files("/media", media)
 
 ui.page("/")(home)
 ui.page("/test/{method}")(wpm_tester_page)
